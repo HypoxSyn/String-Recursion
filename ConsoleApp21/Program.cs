d3c266d48192613751674dc3e72b9e206c757e74
Chaz Hall <chaz.h.hall@gmail.com>
2022-02-14 21:37:17 -0700

    Add project files.

---
+++ ConsoleApp21/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Reverse
+{
+    class Program
+    {
+        public static void Main()
+        {
+            string str= "Robert";
+            Reverse stringObj = new Reverse();
+            stringObj.displayReverse(str); 
+            Console.ReadKey();
+        }
+    }
+    class Reverse
+    {
+        public void displayReverse(string str)
+        {
+            if (str.Length > 0)
+            {
+                Console.WriteLine(str);
+                displayReverse(str.Substring(1, str.Length - 1));
+               
+            }
+            else
+            {
+                return;
+            }
+            Console.Write(str[0]);
+        }
+    }
+}
